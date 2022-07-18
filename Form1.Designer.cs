c4e0c9186a6ab9c038a0245380d7b10575e88580
JHP <uk_admin@jprogramme.com>
2022-07-19 00:09:47 +0900

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace ChromeHistoryBrowser
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.dtHeader = new System.Windows.Forms.ColumnHeader();
+            this.urlHeader = new System.Windows.Forms.ColumnHeader();
+            this.titleHeader = new System.Windows.Forms.ColumnHeader();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "날짜 : ";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(80, 9);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(300, 31);
+            this.dateTimePicker1.TabIndex = 1;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(432, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "검색 : ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(500, 9);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(556, 31);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(1072, 11);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(138, 29);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "날짜 내 검색";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1227, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 34);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "검색";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.dtHeader,
+            this.urlHeader,
+            this.titleHeader});
+            this.listView1.FullRowSelect = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 49);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(1581, 1086);
+            this.listView1.TabIndex = 6;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // dtHeader
+            // 
+            this.dtHeader.Text = "날짜/시간";
+            this.dtHeader.Width = 250;
+            // 
+            // urlHeader
+            // 
+            this.urlHeader.Text = "URL";
+            this.urlHeader.Width = 400;
+            // 
+            // titleHeader
+            // 
+            this.titleHeader.Text = "제목";
+            this.titleHeader.Width = 400;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1345, 9);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(167, 34);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "저장";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1605, 1147);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Chrome 사용 기록 조회";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader dtHeader;
+        private System.Windows.Forms.ColumnHeader urlHeader;
+        private System.Windows.Forms.ColumnHeader titleHeader;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+using System.IO;
+
+namespace ChromeHistoryBrowser
+{
+    public partial class Form1 : Form
+    {
+        private SQLiteConnection historyFile;
+        private DirectoryInfo chromeDir;
+        private const string appPath = "\\Google\\Chrome\\User Data\\Default";
+        public Form1()
+        {
+            InitializeComponent();
+            this.FormClosing += Form1_FormClosing;
+            this.textBox1.KeyPress += TextBox1_KeyPress;
+        }
+
+        private void TextBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar == 13)
+            {
+                updateRecords();
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if(historyFile.State == ConnectionState.Open)
+            {
+                historyFile.Close();
+                GC.Collect();
+                GC.WaitForPendingFinalizers(); // Close 이후 메모리 상에서 없어지기 전까지 대기해야 아래 열었던 복사본을 삭제할 수 있음.
+            } 
+
+            if (File.Exists(chromeDir.FullName + appPath + "\\History_copy"))
+            {
+                File.Delete(chromeDir.FullName + appPath + "\\History_copy");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            chromeDir = new DirectoryInfo(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData));
+            if(!File.Exists(chromeDir.FullName + appPath + "\\History"))
+            {
+                MessageBox.Show("정보를 찾을 수 없습니다.");
+                Application.Exit();
+                return;
+            }
+
+            File.Copy(chromeDir.FullName + appPath + "\\History", chromeDir.FullName + appPath + "\\History_copy",true); // History는 쓰이고 있으므로 복사 후 사용한다.
+
+            historyFile = new SQLiteConnection("Data Source=" + chromeDir.FullName + appPath + "\\History_copy;Version=3;");
+
+            historyFile.Open();
+
+            updateRecords();
+        }
+
+        /// <summary>
+        /// 특정 날짜와 검색 키워드로 
+        /// </summary>
+        private void updateRecords()
+        {
+            string searchKeyword = textBox1.Text, searchQuery = "";
+            DateTime searchDate = dateTimePicker1.Value;
+            SQLiteCommand sqlQuery;
+            SQLiteDataReader sqlRows;
+            ListViewItem newRow;
+
+            if (historyFile.State != ConnectionState.Open)
+            {
+                MessageBox.Show("검색 기록 DB가 열리지 않았습니다.");
+                return;
+            }
+
+            listView1.Items.Clear();
+
+            searchQuery = dateQuery(searchDate);
+
+            if (searchKeyword.Trim().Length > 0)
+            {
+                if (checkBox1.Checked)
+                {
+                    searchQuery = searchQuery + " AND (url LIKE '%" + searchKeyword + "%' or title LIKE '%" + searchKeyword + "%')";
+                }
+                else
+                {
+                    searchQuery = "  (url LIKE '%" + searchKeyword + "%' or title LIKE '%" + searchKeyword + "%')";
+                }
+            }
+
+            using (sqlQuery = new SQLiteCommand("SELECT datetime(last_visit_time/1000000-11644473600,'unixepoch','localtime') lctime, title, url FROM urls WHERE " + searchQuery + " ORDER BY last_visit_time DESC", historyFile)) { 
+
+                sqlRows = sqlQuery.ExecuteReader();
+
+                while (sqlRows.Read())
+                {
+                    newRow = new ListViewItem(new String[3] { sqlRows["lctime"].ToString(), sqlRows["url"].ToString(), sqlRows["title"].ToString() });
+                    listView1.Items.Add(newRow);
+                }
+
+                sqlRows.Close();
+            }
+        }
+
+        private string dateQuery(DateTime? dt)
+        {
+            long sTime, eTime;
+            if (!dt.HasValue) {
+                dt = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day);
+            } else {
+                dt = new DateTime(dt.Value.Year, dt.Value.Month, dt.Value.Day);
+            }
+
+            sTime = ((DateTimeOffset)dt).ToUnixTimeSeconds();
+            eTime = ((DateTimeOffset)dt.Value.AddSeconds(86399)).ToUnixTimeSeconds();
+
+            return " (last_visit_time/1000000-11644473600) between " + sTime.ToString() + " and " + eTime.ToString() + "  ";
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            updateRecords();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            updateRecords();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
